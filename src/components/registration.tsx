442213c9cadf4229809ebd3587184f9408b7f7ed
Igor Parchinski <parczynski@gmail.com>
2023-07-22 01:09:12 +0400

    feat: real registration

--- src/components/registration.tsx
+++ src/components/registration.tsx
@@ -1,12 +1,20 @@
-import { FC } from 'react';
+import { FC, FormEventHandler, useContext, useState } from 'react';
 import styles from './registration.module.scss'
+import { ProviderContext } from '../core/provider';
 
 export const Registration: FC = () => {
 
+	const { register } = useContext( ProviderContext )
+	const [ phone, setPhone ] = useState( '' )
+
+	const submit: FormEventHandler = (e) => {
+		e.preventDefault()
+		register( phone )
+	}
 	return (
 		<>
-			<form id={styles.registration}>
-				<input type="tel" name="phone" id="" maxLength={12} placeholder='phone number' />
+			<form id={styles.registration} onSubmit={submit}>
+				<input type="tel" name="phone" value={phone} onChange={(e) => setPhone(e.currentTarget.value)} maxLength={12} placeholder='phone number' />
 				<button>Set Number</button>
 			</form>
 		</>
