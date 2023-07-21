ad50e9a1c662d92db1ee564038cee3c7255c009b
Igor Parchinski <parczynski@gmail.com>
2023-07-22 01:08:39 +0400

    feat: provicer send messages in json with ids

--- app/sms_provider.py
+++ app/sms_provider.py
@@ -1,12 +1,19 @@
 from fastapi import WebSocket
+
+
 class SMSProvider:
     def __init__(self) -> None:
         self.connections: dict[str,WebSocket] = {}
+        self.registrations = 0
+        self.messages = 0
 
     async def register( self, websocket: WebSocket, phone: str ):
         await websocket.accept()
         self.connections[phone] = websocket
+        self.registrations+=1
+        await websocket.send_json( { "id": f'{self.registrations}_registration', "content":f'{phone} зарегистрирован' })
 
     async def send( self, phone: str, message: str ):
+        self.messages += 1
         if phone in self.connections:
-            await self.connections[phone].send_text( message )
+            await self.connections[phone].send_json( { "id": f'{self.messages}_message', "content": message })
