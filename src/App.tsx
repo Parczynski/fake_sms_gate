3400b18251218d27a3eac4b1081374351acb5d22
Igor Parchinski <parczynski@gmail.com>
2023-07-22 01:09:29 +0400

    feat: use SMSProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Messages } from './components/messages'
 import { Registration } from './components/registration'
+import { SMSProvider } from './core/provider'
 
 function App() {
   
 
+
   return (
-    <>
+    <SMSProvider>
       <div className="container">
         <Registration />
         <Messages />
       </div>
-    </>
+    </SMSProvider>
   )
 }
 
