a176516e3e91777d49f9393dd2e6e9d0dd241465
Igor Parchinski <parczynski@gmail.com>
2023-07-24 01:04:20 +0400

    feat: add phone label

--- src/components/registration.tsx
+++ src/components/registration.tsx
@@ -14,7 +14,8 @@ export const Registration: FC = () => {
 	return (
 		<>
 			<form id={styles.registration} onSubmit={submit}>
-				<input type="tel" name="phone" value={phone} onChange={(e) => setPhone(e.currentTarget.value)} maxLength={12} placeholder='phone number' />
+				<label htmlFor="phone">Phone number:</label>
+				<input type="tel" name="phone" id="phone" value={phone} onChange={(e) => setPhone(e.currentTarget.value)} maxLength={12} placeholder='+79000000000' />
 				<button>Set Number</button>
 			</form>
 		</>
